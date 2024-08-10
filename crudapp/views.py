817e2c2e16db2e727961a904cb4e79da649bf09f
sivasriraj43 <shivasriraj43@gmail.com>
2024-08-10 11:25:58 +0530

    Result came

--- crudapp/views.py
+++ crudapp/views.py
@@ -10,34 +10,34 @@ def orderFormView(request):
         if form.is_valid():
             form.save()
             return redirect('show_url')
-    template_name = 'crudapp/order.html'
+    template_name = 'orders.html'
     context = {'form': form}
     return render(request, template_name, context)
 
 
 def showView(request):
     obj = Orders.objects.all()
-    template_name = 'crudapp/show.html'
+    template_name = 'show.html'
     context = {'obj': obj}
     return render(request, template_name, context)
 
 def updateView(request, f_oid):
-    obj = Orders.objects.get(oid=f_oid)
+    obj = Orders.objects.get(sno=f_oid)
     form = OrderForm(instance=obj)
     if request.method == 'POST':
         form = OrderForm(request.POST, instance=obj)
         if form.is_valid():
             form.save()
             return redirect('show_url')
-    template_name = 'crudapp/order.html'
+    template_name = 'orders.html'
     context = {'form': form}
     return render(request, template_name, context)
 
 def deleteView(request, f_oid):
-    obj = Orders.objects.get(oid=f_oid)
+    obj = Orders.objects.get(sno=f_oid)
     if request.method == 'POST':
         obj.delete()
         return redirect('show_url')
-    template_name = 'crudapp/confirmation.html'
+    template_name = 'confirmation.html'
     context = {'obj': obj}
     return render(request, template_name, context)
--- crudproject/settings.py
+++ crudproject/settings.py
@@ -55,11 +55,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'crudproject.urls'
-
+import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
